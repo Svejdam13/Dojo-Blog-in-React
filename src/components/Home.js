fb6763f96aed89cd1fe192914aed61a643b3be1d
Martin Svejda <svejdam13@gmail.com>
2021-03-08 20:04:50 +0100

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,22 @@
+import { useState } from 'react'
+
 const Home = () => {
-  const handleClick = (e) => {
-    console.log('Hi Martin', e);
-  }
-  const handleClickAgain = (name) => {
-    console.log(`hello ${name}`);
-  }
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
+
   return ( 
     <div className="home">
-      <h1>home sec</h1>
-      <button onClick={handleClick}>Click here</button>
-      <button onClick={ () => handleClickAgain('Martin')}>Click me</button>
+      {blogs.map((blg) => (
+        <div className="blg-preview" key={blg.id}>
+          <h2>{ blg.title }</h2>
+          <p>Written by { blg.author }</p>
+        </div>
+      ))}
     </div>
    );
 }
  
-export default Home;
+export default Home
