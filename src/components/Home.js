f7505e32923d771b623ae396d50e0baf32d7edd0
Martin Svejda <svejdam13@gmail.com>
2021-03-12 20:35:57 +0100

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,20 +3,32 @@ import BlogList from './BlogList'
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
-
-
+  const [isPending, setIsPending] = useState(true)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs').then(res => {
+    fetch('http://localhost:8000/blogs')
+    .then(res => {
+      if(!res.ok) {
+        throw Error('could not fetch the data...')
+      }
       return res.json()
     })
     .then((data) => {
       setBlogs(data)
+      setIsPending(false)
+      setError(null)
+    })
+    .catch(err => {
+      setError(err.message)
+      setIsPending(false)
     })
   }, [])
 
   return ( 
     <div className="home">
+      { error && <div>{ error }</div>}
+      { isPending && <div>Loading...</div>}
       { blogs && <BlogList blgs={blogs} title="All Blogs" />}
       {/* <BlogList blgs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's blogs" /> */}
     </div>
